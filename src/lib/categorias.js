55a8192b78033975d93664583f042b2a02908789
cavb1205 <cavb.1205@hotmail.com>
2025-02-19 19:32:06 -0300

    create functions for categorys

---
+++ src/lib/categorias.js
@@ -0,0 +1,26 @@
+const { API_URL, CONSUMER_KEY, CONSUMER_SECRET } = process.env;
+
+
+export const getCategorias = async () => {
+    const response = await fetch(
+        `${API_URL}/products/categories?per_page=100&hide_empty=true&consumer_key=${CONSUMER_KEY}&consumer_secret=${CONSUMER_SECRET}`
+    );
+    if (!response.ok) {
+        throw new Error("No se pudo obtener las categorías");
+    }
+    
+    const data = await response.json();
+    return data;
+    }
+
+export const getCategoria = async (slug) => {
+    const response = await fetch(
+        `${API_URL}/products/categories?slug=${slug}&consumer_key=${CONSUMER_KEY}&consumer_secret=${CONSUMER_SECRET}`
+    );
+    if (!response.ok) {
+        throw new Error("No se pudo obtener la categoría");
+    }
+    
+    const data = await response.json();
+    return data;
+    }

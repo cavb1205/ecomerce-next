cf55d4c5030f9f49407514f36a6f51819d39ca59
cavb1205 <cavb.1205@hotmail.com>
2025-02-21 20:03:05 -0300

    menu mobil

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Search from "./Search";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
   const pathname = usePathname();
+  const toggleMobileMenu = () => setIsOpenMobileMenu(!isOpenMobileMenu);
 
   const links = [
     { href: "/", label: "Inicio" },
@@ -15,6 +18,25 @@ export default function Header() {
   ];
   return (
     <header className="mb-10 max-w-[100vw]">
+      <button
+        onClick={toggleMobileMenu}
+        className="block md:hidden text-gray-500 hover:text-pink-400 focus:outline-none mr-2 my-2 mx-auto"
+      >
+        <svg
+          className="w-10 h-10"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16m-7 6h7"
+          ></path>
+        </svg>
+      </button>
       <div className="flex items-center justify-center">
         <Link href="/">
           <Image
@@ -27,7 +49,10 @@ export default function Header() {
           />
         </Link>
       </div>
-      <nav className="flex items-center justify-center bg-pink-100 p-2">
+      <div className="md:hidden text-xl font-semibold text-gray-500 bg-pink-200 text-center p-2">
+        <Search />
+      </div>
+      <nav className="hidden md:flex md:items-center md:justify-center bg-pink-100 p-2">
         <ul className="flex items-center gap-5 text-xl font-semibold">
           {links.map(({ href, label }) => (
             <li key={href}>
@@ -44,10 +69,31 @@ export default function Header() {
             </li>
           ))}
         </ul>
-        <div className="text-xl font-semibold text-gray-500 ml-20"> 
+        <div className="text-xl font-semibold text-gray-500 ml-20">
           <Search />
         </div>
       </nav>
+      {isOpenMobileMenu && (
+        <div className="md:hidden bg-pink-100 p-4">
+          <ul className="flex flex-col gap-3 text-xl font-semibold">
+            {links.map(({ href, label }) => (
+              <li key={href}>
+                <Link
+                  href={href}
+                  className={`${
+                    pathname === href
+                      ? "text-pink-400 font-bold"
+                      : "text-gray-500"
+                  } hover:text-pink-400 hover:scale-105 hover:font-bold inline-block`}
+                  onClick={toggleMobileMenu} // Cerrar el menú al hacer clic en un enlace
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </header>
   );
 }

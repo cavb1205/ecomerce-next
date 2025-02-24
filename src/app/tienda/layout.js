d88177242dc38335e4c19b5b51886b07735abb7d
cavb1205 <cavb.1205@hotmail.com>
2025-02-24 15:10:57 -0300

    suspense menu category list

--- src/app/tienda/layout.js
+++ src/app/tienda/layout.js
@@ -1,18 +1,19 @@
 import MenuCategory from "@/components/MenuCategory";
+
 import MenuCategorySkeleton from "@/components/MenuCategorySkeleton";
 import ProductSkeleton from "@/components/ProductSkeleton";
-import { getCategorias } from "@/lib/categorias";
+
 import { Suspense } from "react";
 
-export default async function Layout({ children }) {
-  let categorias = [];
-  categorias = await getCategorias();
-
+export default function Layout({ children }) {
   return (
     <div className="flex flex-row  justify-center w-full min-h-screen h-auto">
       <div className="py-4 px-6 w-1/6 hidden lg:block ">
-        <MenuCategory categorias={categorias} />
+        <Suspense fallback={<MenuCategorySkeleton />}>
+          <MenuCategory />
+        </Suspense>
       </div>
+
       <div className="w-full lg:w-5/6 ">
         <Suspense fallback={<ProductSkeleton />}>{children}</Suspense>
       </div>

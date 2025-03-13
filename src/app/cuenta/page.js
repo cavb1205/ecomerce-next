9f1145c8404bc1464bffa53ef6135cd2d68b9d2a
cavb1205 <cavb.1205@hotmail.com>
2025-03-13 15:00:18 -0300

    add page account

--- src/app/cuenta/page.js
+++ src/app/cuenta/page.js
@@ -1,17 +1,21 @@
+"use client";
 import RegisterClient from "@/components/RegisterClient";
+import {redirect} from "next/navigation";
 
 export default function Cuenta() {
-    const client = false
-    if (!client) {
-        return (
-            <RegisterClient />
-        );
-    }
-  return (
-    <section className="container mx-auto h-fit  my-14 p-6">
-      <div>
-        <h1>Esto es la página de cuenta</h1>
-      </div>
-    </section>
-  );
+    
+  const client = false;
+  if (!client) {
+    redirect("/login");
+  }
+  if (client) {
+    return (
+      <section className="container mx-auto h-fit  my-14 p-6">
+        <div>
+          <h1>Esto es la página de cuenta</h1>
+        </div>
+      </section>
+    );
+  }
 }
+

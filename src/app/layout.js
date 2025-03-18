cce4328ebcc711e0268be5007bf34aa5af71f144
cavb1205 <cavb.1205@hotmail.com>
2025-03-18 17:20:47 -0300

    add metadata for SEO optimization in RootLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,6 +25,8 @@ export const metadata = {
   title: "Diva Store",
   description:
     "Diva Store Boutique - Tu destino de moda en Calama, Chile. Descubre nuestra exclusiva colección de ropa, diseñada para resaltar la belleza y elegancia de la mujer moderna. ¡Encuentra tu estilo con nosotros!",
+  image: "/public/img/diva-store-logo.jpeg",
+  url: "https://www.divastore.cl",
 };
 
 export default function RootLayout({ children }) {
@@ -32,6 +34,19 @@ export default function RootLayout({ children }) {
     <CartProvider>
       <html lang="es">
         <Analytics />
+        <head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+          <meta property="og:title" content={metadata.title} />
+          <meta property="og:description" content={metadata.description} />
+          <meta property="og:image" content={metadata.image} />
+          <meta property="og:url" content={metadata.url} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={metadata.title} />
+          <meta name="twitter:description" content={metadata.description} />
+          <meta name="twitter:image" content={metadata.image} />
+        </head>
+
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >

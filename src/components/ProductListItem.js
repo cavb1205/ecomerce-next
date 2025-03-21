5707777498623db3d25dd6b4ef2d28e80489f3fd
cavb1205 <cavb.1205@hotmail.com>
2025-03-21 11:54:07 -0300

    add quantity handling and variation selection in ProductListItem component

--- src/components/ProductListItem.js
+++ src/components/ProductListItem.js
@@ -3,6 +3,22 @@ import Link from "next/link";
 import Image from "next/image";
 import { useCart } from "@/lib/CartContext";
 export default function ProductListItem({ producto }) {
+  const handleAddToCart = () => {
+    const productWhitQuantity = {
+      ...producto,
+      quantity : 1,
+    };
+    if (producto.type === "variable") {
+      if (!selectedVariation) {
+        toast.info("Debes seleccionar una variación");
+        return;
+      }
+      productWhitQuantity.variaciones = [selectedVariation];
+      addToCart(productWhitQuantity);
+      return;
+    }
+    addToCart(productWhitQuantity);
+  };
   const { addToCart } = useCart();
   return (
     <div className="max-h-auto">
@@ -26,7 +42,7 @@ export default function ProductListItem({ producto }) {
         </p>
         {producto.type === "simple" ? (
           <button
-            onClick={() => addToCart(producto)}
+            onClick={handleAddToCart}
             className="bg-pink-400 text-white font-semibold py-2 px-5 rounded-md hover:opacity-70 hover:scale-105 cursor-pointer"
           >
             <span>Agrgar al carrito</span>

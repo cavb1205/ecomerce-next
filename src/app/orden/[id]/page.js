6eb6e9a7e82710549f5c32ad969c0ebf42011e41
cavb1205 <cavb.1205@hotmail.com>
2025-03-22 20:04:04 -0300

    style(OrderDetail): improve layout and typography for order details display

--- src/app/orden/[id]/page.js
+++ src/app/orden/[id]/page.js
@@ -5,11 +5,11 @@ export default async function OrderDetail({ params }) {
   const order = await getOrden(id);
   console.log(order);
   return (
-    <div className="container mx-auto h-fit  my-14 p-6">
+    <div className="container mx-auto h-fit  my-6 p-6">
       <h1 className="text-primary text-2xl md:text-4xl my-4 text-center font-bold">
         Detalles del pedido
       </h1>
-      <p className="text-lg text-secondary">
+      <p className="text-md md:text-lg text-secondary">
         El pedido{" "}
         <span className="text-primary font-semibold">#{order.id}</span> se
         realizó el {order.date_created} y se encuentra{" "}
@@ -17,40 +17,48 @@ export default async function OrderDetail({ params }) {
       </p>
 
       <div className="bg-pink-100/40 rounded-lg p-4 my-4 flex flex-col gap-2">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4  w-full mx-auto mb-4">
-          <h3 className="text-lg text-primary font-semibold">Producto</h3>
-          <h3 className="text-lg text-primary font-semibold">Total</h3>
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-4  w-full mx-auto mb-4">
+          <h3 className="text-md md:text-lg text-primary font-semibold">
+            Producto
+          </h3>
+          <h3 className="text-md md:text-lg text-primary font-semibold">
+            Total
+          </h3>
         </div>
         {order.line_items.map((item) => (
           <div
             key={item.id}
-            className="grid grid-cols-1 md:grid-cols-2 gap-4  w-full mx-auto"
+            className="grid grid-cols-2 md:grid-cols-2 gap-4  w-full mx-auto"
           >
-            <h3 className="text-md text-secondary font-semibold">
-              {item.name}
-            </h3>
+            <div>
+              <h3 className="text-sm text-secondary font-semibold">
+                {item.name}{" "}
+              </h3>
+                <span className="text-xs text-secondary">Qty: {item.quantity}</span>{" "}
+            </div>
             <div className="text-secondary flex justify-start gap-4">
-              <span className="text-md">Qty: {item.quantity}</span>{" "}
-              <span className="text-md text-secondary font-semibold">
+              <span className="text-sm text-secondary font-semibold">
                 {" "}
                 ${item.total}
               </span>
             </div>
           </div>
         ))}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4  w-full mx-auto">
-          <h3 className="text-md text-primary font-semibold">Envío</h3>
-          <h3 className="text-md text-primary font-semibold">
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-4  w-full mx-auto">
+          <h3 className="text-sm md:text-md text-primary font-semibold">Envío</h3>
+          <h3 className="text-sm md:text-md text-primary font-semibold">
             ${order.shipping_total}
           </h3>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4  w-full mx-auto">
-          <h3 className="text-xl text-primary font-semibold">Total</h3>
-          <h3 className="text-xl text-primary font-semibold">${order.total}</h3>
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-4  w-full mx-auto">
+          <h3 className="text-md md:text-xl text-primary font-semibold">Total</h3>
+          <h3 className="text-md md:text-xl text-primary font-semibold">${order.total}</h3>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4  w-full mx-auto">
-          <h3 className="text-base text-secondary font-semibold">Metodo de pago</h3>
-          <h3 className="text-base text-secondary font-semibold">
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-4  w-full mx-auto">
+          <h3 className="text-sm md:text-base text-secondary font-semibold">
+            Metodo de pago
+          </h3>
+          <h3 className="text-sm md:text-base text-secondary font-semibold">
             {order.payment_method_title}
           </h3>
         </div>
@@ -71,7 +79,6 @@ export default async function OrderDetail({ params }) {
         <p className="text-md text-secondary font-semibold">
           País: {order.shipping.country}
         </p>
-
       </div>
     </div>
   );

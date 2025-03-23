2ff28c1b6315b29628b90456266393f71108edf6
cavb1205 <cavb.1205@hotmail.com>
2025-03-22 22:22:23 -0300

    feat(Checkout): refactor client data fetching and order handling logic

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-
 import { useCart } from "@/lib/CartContext";
+
 import FormCheckout from "@/components/FormCheckout";
 import ProductResumeCheckout from "@/components/ProductResumeCheckout";
 
@@ -11,15 +11,15 @@ import SkeletonCheckout from "@/components/SkeletonCheckout";
 import Payments from "@/components/Payments";
 import { toast } from "sonner";
 export default function Checkout() {
-  // const { cartItems } = useCart();
   const router = useRouter();
-  const storedUser = localStorage.getItem("user") || null;
-  const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
 
   const [selectedShipping, setSelectedShipping] = useState("calama");
   const [paymentMethod, setPaymentMethod] = useState(null);
-  const [cliente, setCliente] = useState(parseInt(storedUser) || 0);
+  const [cliente, setCliente] = useState(0);
   const [loading, setLoading] = useState(true);
+  // const [cartItems, setCartItems] = useState([]);
+
+  const {cartItems} = useCart();
 
   const shippingCost = {
     calama: "2000",
@@ -78,6 +78,65 @@ export default function Checkout() {
     ],
   });
 
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user") || 0;
+    const storeItems = JSON.parse(localStorage.getItem("cart")) || [];
+    setOrder(
+      {
+        ...order,
+        customer_id: storedUser,
+        line_items: storeItems.map((item) =>
+          item.variaciones
+            ? {
+                product_id: item.id,
+                variation_id: item.variaciones[0]?.id,
+                quantity: item.quantity,
+              }
+            : {
+                product_id: item.id,
+                quantity: item.quantity,
+              }
+        ),
+      },
+      []
+    )
+
+    const fetchClient = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/customers/${storedUser}?consumer_key=${process.env.NEXT_PUBLIC_CONSUMER_KEY}&consumer_secret=${process.env.NEXT_PUBLIC_CONSUMER_SECRET}`,
+
+          {
+            method: "GET",
+          }
+        );
+        const data = await res.json();
+        console.log("getclient cliente......", data);
+
+        if (data?.code) {
+          // Si hay un código de error en el usuario (por ejemplo, 401 - no autorizado), redirige
+          router.push("/login");
+          return;
+        }
+
+        // Si todo está bien, se actualiza el estado del cliente
+        setCliente(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching client data:", error);
+        // En caso de un error al obtener los datos del cliente, redirige al login
+        router.push("/login");
+      }
+    };
+
+    if (storedUser && storedToken) {
+      fetchClient();
+    }
+    setLoading(false);
+  }, []); 
+
+  
+
   // Función para manejar la selección del método de envío
   const handleShippingChange = (event) => {
     setSelectedShipping(event.target.value);
@@ -124,18 +183,30 @@ export default function Checkout() {
       setLoading(false);
       return;
     }
-    if (!order.billing.first_name || !order.billing.last_name || !order.billing.address_1 || !order.billing.city || !order.billing.postcode) {
+    if (
+      !order.billing.first_name ||
+      !order.billing.last_name ||
+      !order.billing.address_1 ||
+      !order.billing.city ||
+      !order.billing.postcode
+    ) {
       toast.error("Todos los campos de dirección son obligatorios.");
       setLoading(false);
       return;
     }
-  
-    if (!order.shipping.first_name || !order.shipping.last_name || !order.shipping.address_1 || !order.shipping.city || !order.shipping.postcode) {
+
+    if (
+      !order.shipping.first_name ||
+      !order.shipping.last_name ||
+      !order.shipping.address_1 ||
+      !order.shipping.city ||
+      !order.shipping.postcode
+    ) {
       toast.error("Todos los campos de envío son obligatorios.");
       setLoading(false);
       return;
     }
-  
+
     // Verificar si se ha seleccionado un método de pago
     if (!order.payment_method || !order.payment_method_title) {
       toast.error("Debes seleccionar un método de pago.");
@@ -167,45 +238,10 @@ export default function Checkout() {
       toast.success("Pedido realizado con éxito");
       router.push("/orden/" + data.id);
     }
-  }
+  };
   console.log("orden final", order);
 
-  useEffect(() => {
-    
-
-    const fetchClient = async () => {
-      try {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/customers/${storedUser}?consumer_key=${process.env.NEXT_PUBLIC_CONSUMER_KEY}&consumer_secret=${process.env.NEXT_PUBLIC_CONSUMER_SECRET}`,
-
-          {
-            method: "GET",
-          }
-        );
-        const data = await res.json();
-        console.log("getclient cliente......", data);
-
-        if (data?.code) {
-          // Si hay un código de error en el usuario (por ejemplo, 401 - no autorizado), redirige
-          router.push("/login");
-          return;
-        }
-
-        // Si todo está bien, se actualiza el estado del cliente
-        setCliente(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching client data:", error);
-        // En caso de un error al obtener los datos del cliente, redirige al login
-        router.push("/login");
-      }
-    };
-
-    if (storedUser && storedToken) {
-      fetchClient();
-    }
-    setLoading(false);
-  }, []); // Solo s
+  
 
   if (loading) {
     return (
@@ -247,8 +283,7 @@ export default function Checkout() {
           <Payments onPaymentChange={handleSelectedPayment} />
           <button
             onClick={handleSubmit}
-            disabled={loading
-            }
+            disabled={loading}
             className="w-full bg-primary text-white font-bold p-4 text-xl rounded-lg mt-4 hover:opacity-90 hover:scale-105 transition-all"
           >
             Realizar pedido

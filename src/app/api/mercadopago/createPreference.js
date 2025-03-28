178867a697e8327b263ad7b11f3d38276af4514c
cavb1205 <cavb.1205@hotmail.com>
2025-03-28 20:05:25 -0300

    fix(MercadoPago): update payment retrieval to accept an object for improved parameter handling

--- src/app/api/mercadopago/createPreference.js
+++ src/app/api/mercadopago/createPreference.js
@@ -42,8 +42,9 @@ export default async function createPreference(order) {
 
 export async function Pagos(id) {
   console.log("id de la funcion pagosss", id);
+  console.log("tipo de dato id", typeof id);
   try {
-    const payment = await new Payment(client).get( id );
+    const payment = await new Payment(client).get( {'id':id} );
     console.log("obtenemos el pago");
     console.log("Resultado de payment:", JSON.stringify(payment, null, 2));
     // Mapeo de estados de Mercado Pago a WooCommerce
